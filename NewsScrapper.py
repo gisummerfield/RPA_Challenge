c83200624672a986d033a4f58c47d7f60f53423e
gg <gisummerfielda380@gmail.com>
2024-04-18 14:18:30 +0200

    Ensure that PEP8 is followed.

--- NewsScrapper.py
+++ NewsScrapper.py
@@ -9,97 +9,157 @@ import stat
 import re
 from openpyxl import Workbook
 from dateutil.relativedelta import relativedelta
+import requests
 
 
 class NewsScrapper:
 
-    months = {
-        "Jan": "January",
-        "Feb": "February",
-        "Mar": "March",
-        "Apr": "April",
-        "May": "May",
-        "Jun": "June",
-        "Jul": "July",
-        "Aug": "August",
-        "Sep": "September",
-        "Oct": "October",
-        "Nov": "November",
-        "Dec": "December"
-    }
-
     def __init__(self):
+        """
+        Used to extract information of news articles based on a search term for the last x months.
 
+        Currently the NewsScrapper only works for the LA Times website.
+        """
+        #
         self.logger = logging.getLogger(__name__)
+        logging.info("Started")
 
+        # List of all the articles, will be populated with lists.
         self.articles = []
 
-        logging.info("Started")
         self.browser = Selenium()
-        self.browser.open_available_browser(url="https://www.latimes.com/", maximized=True,
-                                       options={"arguments": ['--incognito']})
+        self.browser.open_available_browser(
+            url="https://www.latimes.com/",
+            maximized=True,
+            options={
+                "arguments": ['--incognito']})
+
+        # This variable is used to ensure that a search cannot occur
+        # if the browser is not at the homepage of the website.
         self.at_homepage = True
+
+        # This is the name of the directory where the images are
+        # saved and is used as the name of the excel file.
         self.file_name = ""
 
-        time.sleep(2)
+        time.sleep(2)       # Wait a little extra to ensure everything is loaded before a search occurs.
 
-    def string_contains_money(self, string):
-        # Regular expression pattern to match different money formats
+    @staticmethod
+    def string_contains_money(string):
+        """
+        Return a bool dependant on if a string contains a dollar value.
+        Looks for one of the following formats:
+        $11.1 | $111,111.11 | 11 dollars | 11 USD
+        """
+        # Regular expression pattern to match different money formats.
         money_pattern = r'\$[\d,.]+|\b\d+\s*(?:dollars|USD)\b'
 
         # Search for the pattern in the string
         matches = re.findall(money_pattern, string)
 
-        # If matches are found, return True, otherwise False
+        # If matches are found, return True, otherwise False.
         return bool(matches)
 
-    def get_start_of_search_range(self, months):
+    @staticmethod
+    def get_start_of_search_range(months):
+        """
+        Gets the start of the search range (date) according to the number of search months.
+
+        Returns a datetime object.
+        """
         if months < 2:
             return datetime(datetime.now().year, datetime.now().month, 1)
         else:
-            return datetime(datetime.now().year, datetime.now().month, 1) - relativedelta(months=months - 1)
+            return datetime(datetime.now().year, datetime.now(
+            ).month, 1) - relativedelta(months=months - 1)
 
     def extract_article_elements(self, element_html, direcory, search_phrase):
+        """
+        Extracts various article elements from the article HTML.
+
+        The entire HTML of the article element is parsed to the BeautifulSoup library.
+        BeautifulSoup extracts the title, description, date and image source url.
+
+        Extracted article elements are return as a list in the order:
+        title, date, description, image_filename, phrase_count, contains_money
+
+        Currently only works for the LA Times website.
+        """
+
         soup = BeautifulSoup(element_html, 'html.parser')
 
-        # Extracting title of the article
+        # Extracting title of the article.
         title = soup.find('h3', class_='promo-title').text.strip()
 
-        # Extracting description of the article
+        # Extracting description of the article.
         description = soup.find('p', class_='promo-description').text.strip()
 
-        # Extracting date of the article
+        # Extracting date of the article.
         date = soup.find('p', class_='promo-timestamp').text.strip()
 
-        # Extracting picture filename
-        picture_src = soup.find('img')['src']
-        picture_filename = picture_src.split('%2F')[-1]
+        # Extracting image filename.
+        image_source = soup.find('img')['src']
+        image_filename = image_source.split('%2F')[-1]
 
-        self.browser.capture_element_screenshot("xpath://img[@src=\"" + picture_src + "\"]", direcory + "/"
-                                                + picture_filename + ".jpg")
+        # Saving image.
+        self.browser.capture_element_screenshot(
+            "xpath://img[@src=\"" + image_source + "\"]",
+            direcory + "/" + image_filename + ".jpg")
 
-        contains_money = self.string_contains_money(title) or self.string_contains_money(description)
+        # Determining if the title or description contains a dollar value.
+        contains_money = self.string_contains_money(
+            title) or self.string_contains_money(description)
 
-        phrase_count = (title.lower() + description.lower()).count(search_phrase.lower())
+        # Counting the number of occurrences of the search phrase in the title or description.
+        phrase_count = (title.lower() + description.lower()
+                        ).count(search_phrase.lower())
 
-        return [title, date, description, picture_filename, str(phrase_count), str(contains_money)]
-
+        return [title, date, description, image_filename,
+                str(phrase_count), str(contains_money)]
 
     def search(self, search_phrase: str, search_range: int):
-        # Return empty list if a search is attempted while not at the homepage
+        """
+        Initiates a search for articles based on the search
+        phrase and search range (months) provided.
+
+        The following steps are followed during the search:
+
+        1. The search bar is first brought up and the search phrase is entered and executed.
+           The "Newest" dropdown item is then selected to display the articles in chronological order.
+        2. The outerHTML of the articles is captured and sent to the extract_article_elements
+           method to extract the needed article elements.
+        3. The date of each article is checked to determine if searching should stop
+           according to the search range.
+        4. If searching should continue then the "next page" button is clicked, steps 2 + 3 are repeated.
+           If searching should stop then the the articles list is returned.
+
+        If the bottom of the search page is reached an annoying banner pops up and does not allow
+        the clicking of the "next page" button. This raises the exception ElementClickInterceptedException.
+        If this exception is raised then the page is reloaded and the next button can be clicked.
+
+
+        Currently only works for the LA Times website.
+        """
+
+
+        # Return empty list if a search is attempted while not at the homepage.
         if not self.at_homepage:
             logging.warning("Tried to search while not at the homepage!")
             return []
 
+        # No longer at the home page
         self.at_homepage = False
         search_phrase = search_phrase
         search_range = search_range
 
+        # Generate a file/folder name based on the current date and search conditions.
         date_today = date.today()
-
-        self.file_name = str(date_today) + "_" + search_phrase + "_" + str(search_range)
+        self.file_name = str(date_today) + "_" + \
+            search_phrase + "_" + str(search_range)
         directory_path = self.file_name
 
+        # Attempt to delete the directory if it already exists.
+        # TODO: Fix this!
         if os.path.exists(directory_path):
             try:
                 os.remove(directory_path)
@@ -113,18 +173,25 @@ class NewsScrapper:
             except Exception as e:
                 print(f"Error creating directory: {e}")
 
-
-
-        logging.info("Searching for news articles with the phrase \'" + search_phrase + "\' from the last"
-                     + str(search_range) + " month(s)")
-        # Click on the search button
-        self.browser.click_button_when_visible("xpath://*[@data-element='search-button']")
-        # Clear the search bar
-        self.browser.clear_element_text("xpath://*[@data-element='search-form-input']")
-        # Type a phrase into the search bar
-        self.browser.input_text("xpath://*[@data-element='search-form-input']", search_phrase)
-        # Hit enter to perform the search
-        self.browser.press_keys("xpath://*[@data-element='search-form-input']", "ENTER")
+        # Execute a search.
+        logging.info(
+            "Searching for news articles with the phrase \'" +
+            search_phrase +
+            "\' from the last" +
+            str(search_range) +
+            " month(s)")
+        # Click on the search button.
+        self.browser.click_button_when_visible(
+            "xpath://*[@data-element='search-button']")
+        # Clear the search bar.
+        self.browser.clear_element_text(
+            "xpath://*[@data-element='search-form-input']")
+        # Type a phrase into the search bar.
+        self.browser.input_text(
+            "xpath://*[@data-element='search-form-input']", search_phrase)
+        # Hit enter to perform the search.
+        self.browser.press_keys(
+            "xpath://*[@data-element='search-form-input']", "ENTER")
 
         time.sleep(2)
 
@@ -134,67 +201,71 @@ class NewsScrapper:
 
         time.sleep(2)
 
+        # Repeat until article date is outside the search range.
         while True:
-
             # Find all article elements
-            article_elements = self.browser.find_elements("xpath://div[@class='promo-wrapper']")
+            article_elements = self.browser.find_elements(
+                "xpath://div[@class='promo-wrapper']")
 
+            # There are usually 10 per page, loop through them.
             for article_element in article_elements:
-                element_html = self.browser.get_element_attribute(article_element, "outerHTML")
+                # Get outerHTML.
+                element_html = self.browser.get_element_attribute(
+                    article_element, "outerHTML")
 
-                article_info = self.extract_article_elements(element_html, directory_path, search_phrase)
-                print(article_info)
+                # Extract element.
+                article_info = self.extract_article_elements(
+                    element_html, directory_path, search_phrase)
 
+                # If the article was very recently published then instead of a date
+                # it might say "5 hours ago". In this case, make the date today.
+                # This might need a revision to make sure of the date.
                 if "ago" in article_info[1]:
                     date_object = datetime.today()
                 else:
                     # Convert date string to datetime variable
-                    print(article_info[1])
                     if article_info[1][3] == ".":
-                        date_object = datetime.strptime(article_info[1], "%b. %d, %Y")
+                        date_object = datetime.strptime(
+                            article_info[1], "%b. %d, %Y")
                     else:
-                        date_object = datetime.strptime(article_info[1], "%B %d, %Y")
+                        date_object = datetime.strptime(
+                            article_info[1], "%B %d, %Y")
 
+                # Check if the latest article date falls outside the range.
+                # Return the articles if it does.
                 if self.get_start_of_search_range(search_range) <= date_object:
                     self.articles.append(article_info)
                 else:
                     logging.info("Done finding aritcles")
                     return self.articles
 
-
-
             time.sleep(3)
 
             try:
                 # Go to next page of the articles
-                self.browser.click_link("xpath://div[@class='search-results-module-next-page']/a")
+                self.browser.click_link(
+                    "xpath://div[@class='search-results-module-next-page']/a")
             except ElementClickInterceptedException:
                 self.browser.reload_page()
                 time.sleep(1)
-                self.browser.click_link("xpath://div[@class='search-results-module-next-page']/a")
-
-
+                self.browser.click_link(
+                    "xpath://div[@class='search-results-module-next-page']/a")
 
     def export_articles_as_excel(self, articles):
-        # Create a new workbook
+        """
+        Exports all the articles as a excel spreadsheet.
+        """
+        # Create a new workbook.
         wb = Workbook()
-
-        # Select the active worksheet
+        # Select the active worksheet.
         ws = wb.active
-
-        # Iterate through the list of lists and write each list as a row
+        # Iterate through the list of articles and write each list as a row.
         for row in articles:
             ws.append(row)
-
-        # Save the workbook
+        # Save the workbook.
         wb.save(self.file_name + ".xlsx")
 
-
-
-
     time.sleep(2)
-
-
 
 
 if __name__ == "__main__":
@@ -205,10 +276,8 @@ if __name__ == "__main__":
 
     LAScrapper = NewsScrapper()
     article_list = LAScrapper.search(search_phrase, search_range)
-
-    print(article_list)
-
-
     LAScrapper.export_articles_as_excel(article_list)
-
     LAScrapper.browser.close_browser()
+
+
+
