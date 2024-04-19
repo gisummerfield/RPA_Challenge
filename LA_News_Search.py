59b293f1bdfb9b31d332523027747b7f9a927f2d
Gary Summerfield <u17012822@tuks.co.za>
2024-04-19 23:07:39 +0200

    Another try

--- LA_News_Search.py
+++ LA_News_Search.py
@@ -9,33 +9,18 @@ Example script showing how to use the NewsScraper class.
 
 LAScraper = NewsScraper()                                       # Create NewsScraper object.
 
-##############
-### Test 1 ###
-##############
 @task
-def search1():
-    input_data = workitems.inputs.current
-    print("Received payload:", input_data.payload)
+def search():
+    for input_data in workitems.inputs:
+        print("Received payload:", input_data.payload)
 
-    # Set search terms.
-    search_phrase = input_data.payload['search_phrase']
-    search_range = input_data.payload['search_range']
+        # Set search terms.
+        search_phrase = input_data.payload['search_phrase']
+        search_range = input_data.payload['search_range']
 
-    article_list = LAScraper.search(search_phrase, search_range)    # Perform a search.
-    LAScraper.export_articles_as_excel(article_list)                # Export articles to Excel file.
-    LAScraper.zip_images()                                          # Zip images.
-    LAScraper.browser.close_browser()                               # Close browser.
+        article_list = LAScraper.search(search_phrase, search_range)    # Perform a search.
+        LAScraper.export_articles_as_excel(article_list)                # Export articles to Excel file.
+        LAScraper.zip_images()                                          # Zip images.
+        LAScraper.reset_for_new_search()                                # Reset the NewsScraper for a new search.
+        LAScraper.browser.close_browser()                               # Close browser.
 
-##############
-### Test 1 ###
-##############
-# Set search terms.
-# search_phrase = "spain"
-# search_range = 2
-#
-# LAScraper.reset_for_new_search()                                # Research the NewsScraper for a new search.
-# article_list = LAScraper.search(search_phrase, search_range)    # Perform a search.
-# LAScraper.export_articles_as_excel(article_list)                # Export articles to Excel file.
-# LAScraper.zip_images()                                          # Zip images.
-#
-# LAScraper.browser.close_browser()                               # Close browser.
